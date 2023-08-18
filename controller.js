e2f427d198b43ebcc6891964a0670dcf3e8f7551
Liul Sintayehu <liulsintayehu7@gmail.com>
2023-08-18 04:06:10 -0800

    updated

--- controller.js
+++ controller.js
@@ -5,28 +5,27 @@ const Model = require('./model')
 const Signup = async (req,res)=>{
     const user = new Model(req.body)
     await user.save()
-    .then((res)=>{
+    .then((response)=>{
         console.log('user inserted');
-        res.send("true")
+        res.json({message:'added'});
+
     })
     .catch((err)=>{
         console.log(err);
-        res.send({message:false})
+        res.json({message:'error'})
     })
 }
 const Login = async (req,res)=>{
      const {name,password} = req.body
      const user = await Model.findOne({name,password})
-     try{
+     
         if(!user){
-            return res.status(401).json({message:'invalid credentials'})
+            return res.status(401).json({message:'invalid'})
         }
 
-        return res.status(200).json({message:'home page display'})
+        return res.status(200).json({message:'exists'})
 
-     }catch(err){
-        console.log(err);
-     }
+    
 
 }
 
