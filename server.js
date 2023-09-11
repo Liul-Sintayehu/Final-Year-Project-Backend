cac51ccecf98dab3e10fd5cbfc22841190dafe80
Liul Sintayehu <liulsintayehu7@gmail.com>
2023-09-11 00:13:12 -0800

    update3

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors())
 const uri = "mongodb+srv://liul:eKwJXBaSPD6sHSCX@cluster0.up98o7c.mongodb.net/practice2?retryWrites=true&w=majority";
 mongoose.connect(uri,{useNewUrlParser:true})
 .then((res)=>{
-    console.log('connected to db');
+    console.log('connected to dbs');
 })
 app.listen(45963,()=>{
     console.log('listening on port 45963');
